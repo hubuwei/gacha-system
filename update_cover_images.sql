817716e97471fc954b0907747a7ffb00993dc8f3
hubuwei <your.email@example.com>
2026-04-19 21:46:39 +0800

    修复ES搜索功能：安装IK分词器和拼音插件，更新settings.json配置，创建games索引

---
+++ update_cover_images.sql
@@ -0,0 +1,6 @@
+USE gacha_system_prod;
+UPDATE games SET cover_image = '/GamePapers/black-myth-wukong.jpg' WHERE id = 1;
+UPDATE games SET cover_image = '/GamePapers/original-god.jpg' WHERE id = 2;
+UPDATE games SET cover_image = '/GamePapers/honor-of-kings.jpg' WHERE id = 3;
+UPDATE games SET cover_image = '/GamePapers/league-of-legends.jpg' WHERE id = 4;
+UPDATE games SET cover_image = '/GamePapers/pubg.jpg' WHERE id = 5;
