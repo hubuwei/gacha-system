2c9d4a612f81377d224e0b4f9831c9e500a59559
hubuwei <your.email@example.com>
2026-04-21 20:42:07 +0800

    Enable Elasticsearch repositories and update production configuration

--- mall-service/src/main/java/com/cheng/mall/MallServiceApplication.java
+++ mall-service/src/main/java/com/cheng/mall/MallServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -26,6 +27,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = {
         "com.cheng.mall.repository",
         "com.cheng.common.repository"
+})
+@EnableElasticsearchRepositories(basePackages = {
+        "com.cheng.mall.es.repository"
 })
 @EnableCaching
 @EnableAsync
